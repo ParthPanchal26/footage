d731e803aadc8ff2032f7f741251364de3ed5157
ParthPanchal26 <parthpanchal2625@gmail.com>
2025-01-03 23:20:44 +0530

    Corrected routing in components

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -8,7 +8,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
     console.log(snippet);
     return (
         <Card sx={{ width: { md: '320px', xs: '100%', boxShadow: 'none', borderRadius: 0 } }}>
-            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Link to={videoId ? `/footage/video/${videoId}` : demoVideoUrl}>
                 <CardMedia
                     image={snippet?.thumbnails?.high?.url}
                     alt={snippet?.title}
@@ -18,12 +18,12 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
             <CardContent
                 sx={{ backgroundColor: "#1e1e1e", height: '106px' }}
             >
-                <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+                <Link to={videoId ? `/footage/video/${videoId}` : demoVideoUrl}>
                     <Typography variant="subtitle1" fontWeight="bold" color="#fff">
                         {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
                     </Typography>
                 </Link>
-                <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+                <Link to={snippet?.channelId ? `/footage/channel/${snippet?.channelId}` : demoChannelUrl}>
                     <Typography variant="subtitle2" fontWeight="bold" color="gray">
                         {snippet?.channelTitle || demoChannelTitle}
                         <CheckCircle sx={{ fontSize: 12, color: 'gray', ml: '5px' }} />
